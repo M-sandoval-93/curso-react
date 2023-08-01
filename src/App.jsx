4d6600df669b437624fcb238c153935b87ed5a9c
M-sandova-93 <sandoval.mario.08@gmail.com>
2023-08-01 17:34:15 -0400

    update in work

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-const MyButton = () => {
-  return <button>I`am a button</button>;
+import PropTypes from "prop-types";
+import imgOne from "./assets/images/img.jpg";
+
+
+// components -------------------
+const MyButton = (props) => {
+
+  const handleEventClick = (contenido) => {
+    console.log("Click en el boton " + contenido);
+  }
+
+  return (
+    <button onClick={() => handleEventClick(props.text)}>
+      {props.text}
+    </button>
+  );
 };
 
-const Access = (props) => {
-  const { name } = props;
 
-  return <h1>My name is {name}</h1>;
-};
 
-const NotAccess = (props) => {
-  const { name } = props;
 
-  return <h1>El usuario {name}, no tiene acceso al sistema</h1>;
-};
+// Prop Types of component ------
+
+MyButton.propTypes = {
+  text: PropTypes.string.isRequired,
+}
+
+const MyList = (props) => {
+  const { fruts } = props;
+
+  return (
+    <div>
+      <ul>
+        {fruts.map((frut, index) => <li key={index}>{frut}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+
+const Access = ({ user }) => (user ? <h4>Online</h4> : <h4>Offline</h4>);
+
+
 
 const App = () => {
   const title = "Mi titulo desde una constante";
-  const urlImage = "https://picsum.photos/200/300";
+  // const urlImage = "https://picsum.photos/200/300";
+  const pathImg = "src/assets/images/img.jpg";
   const usuario = false;
+  const fruts = ['manzana', 'pera', 'frutilla'];
 
   return (
     <>
       <h1 className="text-center">{title.toUpperCase()}</h1>
-      <img src={urlImage} alt={`imagen-${title}`} className="img-view" />
-      <MyButton />
+      <img src={imgOne} alt={`imagen-${title}`} className="img-view" />
+      <MyButton text="Boton 1" />
+      <MyButton text="Boton 2" />
+      <MyButton text="Boton 3" />
 
-      {
-        usuario ? <Access name="Mario Sandoval" /> : <NotAccess name="Carlos Sandova" />
-      }
+      <Access user={usuario} />
+      <MyList fruts={fruts} />
     </>
   );
 };
