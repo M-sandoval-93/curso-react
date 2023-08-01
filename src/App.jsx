a0aaa02a3a4daf581cb2a99d64c06c06da4b3e8c
M-sandoval-93 <sandoval.mario.08@gmail.com>
2023-08-01 00:38:39 -0400

    update learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const MyButton = () => {
+  return <button>I`am a button</button>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const Access = (props) => {
+  const { name } = props;
+
+  return <h1>My name is {name}</h1>;
+};
+
+const NotAccess = (props) => {
+  const { name } = props;
+
+  return <h1>El usuario {name}, no tiene acceso al sistema</h1>;
+};
+
+const App = () => {
+  const title = "Mi titulo desde una constante";
+  const urlImage = "https://picsum.photos/200/300";
+  const usuario = false;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1 className="text-center">{title.toUpperCase()}</h1>
+      <img src={urlImage} alt={`imagen-${title}`} className="img-view" />
+      <MyButton />
 
-export default App
+      {
+        usuario ? <Access name="Mario Sandoval" /> : <NotAccess name="Carlos Sandova" />
+      }
+    </>
+  );
+};
+
+export default App;
